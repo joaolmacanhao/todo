e90b862ca7b86da952cb4a4aa59abe7584ba4a77
leonardomacanhao <leonardomacanhao@gmail.com>
2025-05-09 20:31:12 -0400

    implementacao de buscas, inclusao de tarefas, exclusao e marcar como concluidas

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css"
+
 import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
+import Search from "./components/Search";
 
 function App() {
 
-  const [todos, setTodos] = useState([
+  const [todos , setTodos] = useState([
 
     {
       id: 1,
@@ -22,16 +24,42 @@ function App() {
     },
 
   ])
+
+  const [search, setSearch] = useState("");
+
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    },]
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ?  todo : null);
+    setTodos(filteredTodos);
+  }
+
+  const completeTodo =  (id) =>{
+    const newTodos = [...todos];
+    newTodos.map((todo) => todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo)
+    setTodos(newTodos)
+  }
   
   return <div className="app">
     <h1>Lista de Tarefas</h1>
+    <Search search={search} setSearch={setSearch}/>
       <div className="todo-list">
-        {todos.map((todo) => (
-          <Todo todo={todo} />
+        {todos.filter((todo) => todo.text.toLocaleLowerCase().includes(search.toLocaleLowerCase())).map((todo) => (
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
         ) )}
       </div>
-        <TodoForm />
+        <TodoForm  addTodo={addTodo}/>
   </div>
+
 }
 
 export default App
--- todo/src/components/TodoForm.jsx
+++ todo/src/components/TodoForm.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import {useState} from 'react'
+import App from '../App';
 
-const TodoForm = () => {
+const TodoForm = ({addTodo}) => {
+
+    const [value,setValue] = useState("");
+    const [ category, setCategory] = useState(""); 
+
+    const handleSubmit = (e) => {
+        e.preventDefault ();
+        console.log (value, category)
+        if (!value || !category) return;
+        addTodo(value, category)
+        setValue("");
+        setCategory("") ;
+        
+    };
+
   return (
     <div className='todo-form'>
         <h2>Criar Tarefa:</h2>
-        <form>
-            <input type="text" placeholder='Digite a tarefa'/>
-            <select>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Digite a tarefa'
+            value = {value}
+            onChange={(e) => setValue (e.target.value)}/>
+            <select 
+            value = {category}
+            onChange={(e) => setCategory (e.target.value)}>
                 <option value="">Selecione a prioridade</option>
                 <option value="Alta">Alta</option>
                 <option value="Média">Média</option>
