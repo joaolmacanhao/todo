74c7e0a81f517523b43e1f1011438ffa24e534c4
Joao Leonardo Macanhao <leonardomacanhao@gmail.com>
2025-05-11 13:11:04 -0400

    implementacao de busca, ordenacao e filtro. Logica de filtro ainda precisa de correcao

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -4,15 +4,14 @@ import "./App.css"
 import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
 import Search from "./components/Search";
+import Filter from "./components/Filter";
 
 function App() {
 
-  const [todos , setTodos] = useState([
-
-
-  ])
-
-  const [search, setSearch] = useState("");
+  const[todos , setTodos] = useState([])
+  const[search, setSearch] = useState("");
+  const[filter, setFilter] = useState("All");
+  const[sort, setSort] = useState ("Asc")
 
   const addTodo = (text, category) => {
     const newTodos = [...todos, {
@@ -39,8 +38,14 @@ function App() {
   return <div className="app">
     <h1>Lista de Tarefas</h1>
     <Search search={search} setSearch={setSearch}/>
+    <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
+
       <div className="todo-list">
-        {todos.filter((todo) => todo.text.toLocaleLowerCase().includes(search.toLocaleLowerCase())).map((todo) => (
+        {todos
+        .filter((todo) => filter === "All" ?  true: filter == "Completed" ? todo.isCompleted : !todo.isCompleted)
+        .filter((todo) => todo.text.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+        .sort((a , b) => sort ==="Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
+        .map((todo) => (
           <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
         ) )}
       </div>
